f5768ccb780c5761966b5ee86f27338bd2499911
mr-kelly <23110388@qq.com>
2016-01-02 17:49:21 +0800

    with test

--- monkeyd/monkeyd.go
+++ monkeyd/monkeyd.go
@@ -3,21 +3,21 @@ package monkeyd
 import (
     "fmt"
     "github.com/pelletier/go-toml"
+    "net"
+    "log"
+    "io/ioutil"
 )
 // Monkeyd class
 type Monkeyd struct {
-	configFile string // toml config file path
     config *toml.TomlTree
 }
 
-// New Monkeyd
-func New(configFile string) *Monkeyd {
+func NewWithContent(configContent string) *Monkeyd {
+
 
     monkeyd := new(Monkeyd)
 
-    monkeyd.configFile = configFile
-
-    config, err := toml.LoadFile(configFile)
+    config, err := toml.Load(configContent)
     if (err != nil) {
         fmt.Println("Error ", err.Error())
         panic(err)
@@ -28,13 +28,83 @@ func New(configFile string) *Monkeyd {
     return monkeyd
 
 }
+// New Monkeyd
+func New(configFile string) (*Monkeyd, error) {
+    content, err := ioutil.ReadFile(configFile)
+    if err != nil {
+         return nil, err
+    }
+    return NewWithContent(string(content)), err
+}
 
 func (this *Monkeyd) Run(sectionStr string) {
-    fmt.Println("section string: %s", sectionStr)
     tree := this.config.Get(sectionStr).(*toml.TomlTree)
     fmt.Println(tree.ToString())
-    for _, key := range tree.Keys() {
-        value := tree.Get(key)
-        fmt.Printf("Key: %s, Value: %s\n", key, value)
+    typeStr := tree.Get("type")
+    if (typeStr == "server") {
+        this.RunServer(tree.Get("forwardPort").(int64), tree.Get("servePort").(int64))
+    } else {
+        panic(fmt.Sprintf("Unknowd type str: %s", typeStr))
     }
+}
+
+
+func (this *Monkeyd) ConnHandler(conn net.Conn) {
+
+    defer conn.Close()
+
+    buf := make([]byte, 10240)
+    for {
+        // Recv
+        _, err := conn.Read(buf)
+        if (err != nil) {
+            log.Printf("Read fail")
+            return
+        }
+
+        // Send reply
+        _, err = conn.Write(buf)
+        if err != nil {
+            log.Printf("Write fail")
+            return
+        }
+
+    }
+}
+
+/*
+运行服务器模式，传入服务端口，和转发到的端口
+*/
+func (this *Monkeyd) RunServer(fowardPort int64, servePort int64) {
+
+    // serve port Conn Channel
+    serveConnChan := make(chan net.Conn)
+
+    go func() {
+        for connChan := range serveConnChan {
+            go this.ConnHandler(connChan)
+        }
+    }()
+
+    // 同步开始监听端口
+    addr := fmt.Sprintf("0.0.0.0:%d", servePort)
+
+    fmt.Println("[RunServer]Server listen: " + addr)
+
+    serveListener, err := net.Listen("tcp", addr)
+    conn, err := serveListener.Accept();
+    if err != nil {
+        log.Printf("Error accept: %s", err)
+        return
+    }
+
+    // 异步不停获取新连接
+    go func() {
+
+        defer serveListener.Close()
+
+        for {
+            serveConnChan <- conn
+        }
+    }()
 }
--- monkeyd/monkeyd_test.go
+++ monkeyd/monkeyd_test.go
@@ -2,8 +2,62 @@ package monkeyd
 
 import (
 	"testing"
+    "net"
+    "fmt"
+    "time"
+    //"bytes"
+    "strings"
 )
 
+func TestStringEqual(t *testing.T) {
+    if !strings.EqualFold("ABC", "ABC") {
+        t.Error("Test string equal fail")
+    }
+}
 func TestNewMonkeyd(t *testing.T) {
 
+    m := NewWithContent(`[test_serve]
+type = "server"
+forwardPort = 3389
+servePort = 33890
+`)
+    signal := make(chan bool)
+    go func() {
+        m.Run("test_serve")
+        signal <- true
+    }()
+
+    fmt.Println("Wait 1s")
+    time.Sleep(1 * time.Second)
+
+    // client -> 用户机
+    client, err := net.Dial("tcp", "127.0.0.1:33890")
+    if err != nil {
+        fmt.Println("error on dial test" + err.Error())
+        return
+    }
+    defer client.Close()
+    testStrArr := []byte("test string")
+    testStr := string(testStrArr)
+    client.Write(testStrArr)
+
+    buf := make([]byte, 11)
+    _, err = client.Read(buf)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    readStr := string(buf)
+    if len(readStr) != len(testStr) {
+        t.Error(fmt.Sprintf("Test string length error, expect: %d, but: %d", len(readStr), len(testStr)))
+    }
+    if !strings.EqualFold(readStr, testStr) {
+        t.Error(fmt.Sprintf("Test error expect: '%s', but: '%s' ", readStr, testStr))
+    }
+
+    // forward -> 转发机
+    // little machine in intranet
+
 }
+
